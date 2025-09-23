819254f82cf337823c88833d9f1095a9eb31af84
김태민 <gtaemin254@gmail.com>
2025-09-23 17:37:40 +0900

    init

---
+++ src/main/java/com/example/crewstation/CrewStationApplication.java
@@ -0,0 +1,13 @@
+package com.example.crewstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewStationApplication.class, args);
+    }
+
+}
