fcf9c8f855c159b52c87296efafbc3d6f64d25a3
김태민 <gtaemin254@gmail.com>
2025-09-25 15:12:50 +0900

    gradle,yml

--- src/main/java/com/example/crewstation/mapper/crew/CrewMapper.java
+++ src/main/java/com/example/crewstation/mapper/crew/CrewMapper.java
@@ -1,5 +1,6 @@
 package com.example.crewstation.mapper.crew;
 
+import com.example.crewstation.domain.crew.CrewVO;
 import com.example.crewstation.dto.crew.CrewDTO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 @Mapper
 public interface CrewMapper {
 //    크루 정보 조회
-    public Optional<CrewDTO> getCrews();
+    public Optional<CrewDTO> getCrews(CrewDTO crewDTO);
 
 
 }
