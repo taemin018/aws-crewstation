03a12692d2705c94c088132ff39a91d26efaa61b
chukaheyo <chukaheyo@gmail.com>
2025-09-25 17:00:51 +0900

    dddddddd

--- src/main/java/com/example/crewstation/domain/ask/askVO.java
+++
@@ -1,4 +0,0 @@
-package com.example.crewstation.domain.ask;
-
-public class askVO {
-}
--- src/main/resources/sql/inquiry.sql
+++ src/main/resources/sql/inquiry.sql
@@ -9,3 +9,8 @@ create table tbl_inquiry
     constraint fk_inquiry_member foreign key (member_id)
         references tbl_member (id)
 );
+
+select * from tbl_inquiry;
+
+insert into tbl_inquiry (inquiry_title, inquiry_content, member_id)
+VALUES ('질문있어요','여행 크루가 연락이 끊겼어요',1);
