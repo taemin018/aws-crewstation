ed740a8c0bdab9d8731d85ee4db2c8ac9abcb8dc
김태민 <gtaemin254@gmail.com>
2025-09-26 13:44:07 +0900

    conflict close

--- src/main/java/com/example/crewstation/mapper/member/MemberMapper.java
+++ src/main/java/com/example/crewstation/mapper/member/MemberMapper.java
@@ -1,20 +1,20 @@
-//package com.example.crewstation.mapper.member;
-//
-//import com.example.crewstation.domain.member.MemberVO;
-//import com.example.crewstation.dto.member.MemberDTO;
-//import org.apache.ibatis.annotations.Mapper;
-//
-//import java.util.Optional;
-//
-//@Mapper
-//public interface MemberMapper {
-//    //    회원가입
-//    public void insert(MemberVO memberVO);
-//
-//    public void insertSns(MemberVO memberVO);
-//    //    로그인
-//    public Optional<MemberDTO> selectForLogin(MemberDTO memberDTO);
-//    //    이메일로 조회
-//    public Optional<MemberDTO> selectMemberByMemberEmail(String memberEmail);
-//    public Optional<MemberDTO> selectMemberBySnsEmail(String snsEmail);
-//}
+package com.example.crewstation.mapper.member;
+
+import com.example.crewstation.domain.member.MemberVO;
+import com.example.crewstation.dto.member.MemberDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper
+public interface MemberMapper {
+    //    회원가입
+    public void insert(MemberVO memberVO);
+
+    public void insertSns(MemberVO memberVO);
+    //    로그인
+    public Optional<MemberDTO> selectForLogin(MemberDTO memberDTO);
+    //    이메일로 조회
+    public Optional<MemberDTO> selectMemberByMemberEmail(String memberEmail);
+    public Optional<MemberDTO> selectMemberBySnsEmail(String snsEmail);
+}
