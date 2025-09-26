be7106d12f39537630f8eac95da50442d65ee5e9
정충효 <alzkdpf000@gmail.com>
2025-09-26 09:41:28 +0900

    purchase yml edit #13

--- src/main/java/com/example/crewstation/auth/JwtAuthenticationHandler.java
+++ src/main/java/com/example/crewstation/auth/JwtAuthenticationHandler.java
@@ -1,28 +1,28 @@
-package com.example.crewstation.auth;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-@Slf4j
-public class JwtAuthenticationHandler implements AuthenticationEntryPoint {
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        log.error("AuthenticationEntryPoint Exception: {}", authException.getMessage());
-        if(request.getRequestURI().startsWith("/api/")){
-//            REST 요청인 경우
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-
-        }else{
-//            일반 웹 요청인 경우
-            response.sendRedirect("/member/login");
-        }
-    }
-}
+//package com.example.crewstation.auth;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//@Slf4j
+//public class JwtAuthenticationHandler implements AuthenticationEntryPoint {
+//    @Override
+//    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//        log.error("AuthenticationEntryPoint Exception: {}", authException.getMessage());
+//        if(request.getRequestURI().startsWith("/api/")){
+////            REST 요청인 경우
+//            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+//
+//        }else{
+////            일반 웹 요청인 경우
+//            response.sendRedirect("/member/login");
+//        }
+//    }
+//}
