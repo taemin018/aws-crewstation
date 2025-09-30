16fe08b411efc6d539d86b21c1e8b4b200219074
chukaheyo <chukaheyo@gmail.com>
2025-09-30 17:24:53 +0900

    temp

--- src/main/java/com/example/crewstation/auth/OAuth2Attribute.java
+++ src/main/java/com/example/crewstation/auth/OAuth2Attribute.java
@@ -17,24 +17,15 @@ public class OAuth2Attribute {
     private Map<String, Object> attributes;
     private String attributeKey;
     private String email;
-<<<<<<< HEAD
     private String password;
-=======
->>>>>>> noticedetail
     private String name;
     private String profile;
     private String provider;
 
     public static OAuth2Attribute of(String provider, String attributeKey, Map<String, Object> attributes){
-<<<<<<< HEAD
 //        log.info("provider" + provider);
 //        log.info("attributeKey" + attributeKey);
 //        log.info("attributes" + attributes.toString());
-=======
-        log.info(provider);
-        log.info(attributeKey);
-        log.info(attributes.toString());
->>>>>>> noticedetail
         switch (provider){
             case "google":
                 return ofGoogle(provider, attributeKey, attributes);
@@ -47,30 +38,24 @@ public class OAuth2Attribute {
         }
     }
 
-//    Google
+    //    Google
     private static OAuth2Attribute ofGoogle(String provider, String attributeKey, Map<String, Object> attributes){
         log.info(attributes.toString());
-<<<<<<< HEAD
 
         return OAuth2Attribute.builder()
                 .email((String)attributes.get("email"))
                 .profile((String)attributes.get("picture"))
                 .name((String)attributes.get("name"))
-=======
-        return OAuth2Attribute.builder()
-                .email((String)attributes.get("email"))
->>>>>>> noticedetail
                 .provider(provider)
                 .attributes(attributes)
                 .attributeKey(attributeKey)
                 .build();
     }
 
-//    Kakao
+    //    Kakao
     private static OAuth2Attribute ofKakao(String provider, String attributeKey, Map<String, Object> attributes){
         Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
         Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
-<<<<<<< HEAD
         log.info("1{}:",kakaoAccount);
         log.info("2{}:",kakaoProfile);
         log.info("3{}:",kakaoAccount.get("email"));
@@ -80,32 +65,21 @@ public class OAuth2Attribute {
         return OAuth2Attribute.builder()
                 .email((String) kakaoAccount.get("email"))
                 .profile((String)kakaoProfile.get("profile_image_url"))
-=======
-
-        log.info(kakaoAccount.toString());
-        log.info(kakaoProfile.toString());
-
-        return OAuth2Attribute.builder()
-                .email((String) kakaoAccount.get("email"))
->>>>>>> noticedetail
                 .provider(provider)
                 .attributes(kakaoAccount)
                 .attributeKey(attributeKey)
                 .build();
     }
 
-//    Naver
+    //    Naver
     private static OAuth2Attribute ofNaver(String provider, String attributeKey, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
         log.info(response.toString());
 
         return OAuth2Attribute.builder()
                 .email((String) response.get("email"))
-<<<<<<< HEAD
                 .profile((String)response.get("profile"))
                 .name((String)response.get("name"))
-=======
->>>>>>> noticedetail
                 .attributes(response)
                 .provider(provider)
                 .attributeKey(attributeKey)
@@ -117,11 +91,8 @@ public class OAuth2Attribute {
         map.put("id", attributeKey);
         map.put("key", attributeKey);
         map.put("email", email);
-<<<<<<< HEAD
         map.put("profile", profile);
         map.put("name", name );
-=======
->>>>>>> noticedetail
         map.put("provider", provider);
 
         return map;
