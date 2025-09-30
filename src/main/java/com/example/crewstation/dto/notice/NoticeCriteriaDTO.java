1ecb0359e3c33fb81ef5468fad567c7fc227b5f1
chukaheyo <chukaheyo@gmail.com>
2025-09-30 12:14:03 +0900

    notice 0930a

---
+++ src/main/java/com/example/crewstation/dto/notice/NoticeCriteriaDTO.java
@@ -0,0 +1,16 @@
+package com.example.crewstation.dto.notice;
+
+import com.example.crewstation.domain.notice.NoticeVO;
+import com.example.crewstation.util.Criteria;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter @Setter
+@ToString
+public class NoticeCriteriaDTO {
+    private List<NoticeVO> noticeLists;
+    private Criteria criteria;
+}
