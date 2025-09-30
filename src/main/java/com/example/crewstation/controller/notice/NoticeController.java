00850e9bb2326be057bdc62c82ade120e72b8342
chukaheyo <chukaheyo@gmail.com>
2025-09-30 17:07:07 +0900

    111

--- src/main/java/com/example/crewstation/controller/notice/NoticeController.java
+++ src/main/java/com/example/crewstation/controller/notice/NoticeController.java
@@ -2,7 +2,7 @@ package com.example.crewstation.controller.notice;
 
 import com.example.crewstation.service.notice.NoticeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/notice/**")
+@RequestMapping("/notice")
 @RequiredArgsConstructor
+@Slf4j   // ✅ log 객체 자동 생성
 public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @GetMapping("notice")
+    @GetMapping("/notice")
     public String list(@RequestParam(defaultValue = "1") int page, Model model) {
+        log.info("📌 /notice/notice 요청 들어옴, page={}", page);
+
+
         model.addAttribute("noticeCriteriaDTO", noticeService.getNotices(page));
+        log.info("📌 noticeCriteriaDTO 추가됨, 데이터={}", model.getAttribute("noticeCriteriaDTO"));
+
         return "notice/notice";
     }
 }
