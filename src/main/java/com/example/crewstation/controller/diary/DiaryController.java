9897ae158a516b833c37a243c59ac8ab791d8f49
정충효 <alzkdpf000@gmail.com>
2025-10-01 10:52:25 +0900

    diary list init #88

--- src/main/java/com/example/crewstation/controller/diary/DiaryController.java
+++ src/main/java/com/example/crewstation/controller/diary/DiaryController.java
@@ -3,9 +3,11 @@ package com.example.crewstation.controller.diary;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("/diaries/**")
 public class DiaryController {
 
     // 마이페이지 -> 내가 좋아요한 일기 목록 화면 & 내가 댓글 단 일기 목록 화면
@@ -13,5 +15,10 @@ public class DiaryController {
     public String loadMyActivitiesPage() {
         log.info("마이페이지 - 좋아요한 일기 화면 요청");
         return "mypage/my-activities";
+    }
+
+    @GetMapping()
+    public String list(){
+        return "diary/list";
     }
 }
